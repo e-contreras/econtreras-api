139110bafb40ff7fb337355f8c4d41bf3b3b06d1
Enrique Rodríguez <erodriguez@consultoresinformaticos.com.py>
2019-08-12 14:54:51 -0400

    First commit

---
+++ src/main/java/py/com/econtreras/econtrerasapi/EcontrerasApiApplication.java
@@ -0,0 +1,13 @@
+package py.com.econtreras.econtrerasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcontrerasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcontrerasApiApplication.class, args);
+	}
+
+}
