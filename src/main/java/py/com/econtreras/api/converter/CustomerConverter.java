e5b3df370281b00b8045a0b21686139a5609ebc6
Miguel Mendoza <mmendoza@consultoresinformaticos.com.py>
2019-11-21 18:03:47 -0300

    ajuste

--- src/main/java/py/com/econtreras/api/converter/CustomerConverter.java
+++ src/main/java/py/com/econtreras/api/converter/CustomerConverter.java
@@ -30,32 +30,31 @@ public class CustomerConverter {
     public py.com.econtreras.api.beans.CustomerResponse buildBean(Customer entity) {
         py.com.econtreras.api.beans.CustomerResponse bean = new py.com.econtreras.api.beans.CustomerResponse();
         bean.setCustomerId(entity.getId());
-        bean.setPersonId(entity.getPerson()!= null?entity.getPerson().getId():null);
-        bean.setBusinessName(entity.getPerson()!= null?entity.getPerson().getBusinessName():null);
-        bean.setName(entity.getPerson()!= null?entity.getPerson().getName():null);
-        bean.setLastname(entity.getPerson()!= null?entity.getPerson().getLastname():null);
-        bean.setPhone(entity.getPerson()!= null?entity.getPerson().getPhone():null);
-        bean.setCellphone(entity.getPerson()!= null?entity.getPerson().getCellphone():null);
-        bean.setEmail(entity.getPerson()!= null?entity.getPerson().getEmail():null);
-        bean.setBirthday(entity.getPerson()!= null?entity.getPerson().getBirthday():null);
-        bean.setDocumentNumber(entity.getPerson()!= null?entity.getPerson().getDocumentNumber():null);
+        bean.setPersonId(entity.getPerson() != null ? entity.getPerson().getId() : null);
+        bean.setBusinessName(entity.getPerson() != null ? entity.getPerson().getBusinessName() : null);
+        bean.setName(entity.getPerson() != null ? entity.getPerson().getName() : null);
+        bean.setLastname(entity.getPerson() != null ? entity.getPerson().getLastname() : null);
+        bean.setPhone(entity.getPerson() != null ? entity.getPerson().getPhone() : null);
+        bean.setCellphone(entity.getPerson() != null ? entity.getPerson().getCellphone() : null);
+        bean.setEmail(entity.getPerson() != null ? entity.getPerson().getEmail() : null);
+        bean.setBirthday(entity.getPerson() != null ? entity.getPerson().getBirthday() : null);
+        bean.setDocumentNumber(entity.getPerson() != null ? entity.getPerson().getDocumentNumber() : null);
         return bean;
     }
-    
+
     public py.com.econtreras.api.beans.CustomerResponse buildBean(Customer entity, Link... links) {
         py.com.econtreras.api.beans.CustomerResponse bean = new py.com.econtreras.api.beans.CustomerResponse();
         bean.setCustomerId(entity.getId());
-        bean.setPersonId(entity.getPerson()!= null?entity.getPerson().getId():null);
-        bean.setBusinessName(entity.getPerson()!= null?entity.getPerson().getBusinessName():null);
-        bean.setName(entity.getPerson()!= null?entity.getPerson().getName():null);
-        bean.setLastname(entity.getPerson()!= null?entity.getPerson().getLastname():null);
-        bean.setPhone(entity.getPerson()!= null?entity.getPerson().getPhone():null);
-        bean.setCellphone(entity.getPerson()!= null?entity.getPerson().getCellphone():null);
-        bean.setEmail(entity.getPerson()!= null?entity.getPerson().getEmail():null);
-        bean.setBirthday(entity.getPerson()!= null?entity.getPerson().getBirthday():null);
-        bean.setDocumentNumber(entity.getPerson()!= null?entity.getPerson().getDocumentNumber():null);
+        bean.setPersonId(entity.getPerson() != null ? entity.getPerson().getId() : null);
+        bean.setBusinessName(entity.getPerson() != null ? entity.getPerson().getBusinessName() : null);
+        bean.setName(entity.getPerson() != null ? entity.getPerson().getName() : null);
+        bean.setLastname(entity.getPerson() != null ? entity.getPerson().getLastname() : null);
+        bean.setPhone(entity.getPerson() != null ? entity.getPerson().getPhone() : null);
+        bean.setCellphone(entity.getPerson() != null ? entity.getPerson().getCellphone() : null);
+        bean.setEmail(entity.getPerson() != null ? entity.getPerson().getEmail() : null);
+        bean.setBirthday(entity.getPerson() != null ? entity.getPerson().getBirthday() : null);
+        bean.setDocumentNumber(entity.getPerson() != null ? entity.getPerson().getDocumentNumber() : null);
         bean.add(links);
         return bean;
     }
 }
-
